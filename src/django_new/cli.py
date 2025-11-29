21c003e129b4d89fc91933752e2c22ccedf05d03
adamghill <adamghill@yahoo.com>
2025-11-29 17:32:38 -0500

    Add --version.

--- src/django_new/cli.py
+++ src/django_new/cli.py
@@ -1,11 +1,19 @@
+import importlib.resources
 import logging
+from importlib.metadata import version
 from pathlib import Path
+from typing import Annotated
 
 import typer
 
 from django_new.creators.app import ApiAppCreator, AppCreator, WebAppCreator, WorkerAppCreator
 from django_new.creators.project import ClassicProjectCreator, MinimalProjectCreator
 from django_new.utils import console, is_running_under_any_uv, stderr, stdout
+
+try:
+    from tomllib import loads as toml_loads
+except ImportError:
+    from tomli import loads as toml_loads
 
 try:
     from django.core.management.base import CommandError
@@ -18,17 +26,47 @@ logger = logging.getLogger(__name__)
 app = typer.Typer(help="Create a new Django project.")
 
 
+def version_callback(show_version: bool) -> None:  # noqa: FBT001
+    """Show the version and exit."""
+
+    if not show_version:
+        return
+
+    version_str = ""
+
+    try:
+        version_str = version("django-new")
+    except ImportError:
+        logger.debug("Could not get version from importlib.metadata, so falling back to reading pyproject.toml")
+
+        try:
+            resource = importlib.resources.files("django_new").parent.parent / "pyproject.toml"
+            version_str = toml_loads(resource.read_text()).get("project", {}).get("version")
+        except Exception as e:
+            logger.error("Failed to read version from pyproject.toml", exc_info=e)
+
+    if version_str:
+        typer.echo(f"django-new v{version_str}")
+    else:
+        typer.echo("django-new (version unknown)")
+
+    raise typer.Exit()
+
+
 def create_project(
     name: str = typer.Argument(..., help="Project name"),
     folder: str = typer.Argument(
         ".", help="Optional project folder to create the project in. Defaults to the current directory."
     ),
-    project: bool = typer.Option(False, "--project", "-p", help="Create a project without an app."),
-    minimal: bool = typer.Option(False, "--minimal", "-m", help="Create a minimal project."),
-    app: bool = typer.Option(False, "--app", help="Create a default app."),
-    web: bool = typer.Option(False, "--web", help="Create a website."),
-    api: bool = typer.Option(False, "--api", help="Create an API."),
-    worker: bool = typer.Option(False, "--worker", help="Create a worker."),
+    project: bool = typer.Option(False, "--project", "-p", help="Create a project without an app."),  # noqa: FBT001
+    minimal: bool = typer.Option(False, "--minimal", "-m", help="Create a minimal project."),  # noqa: FBT001
+    app: bool = typer.Option(False, "--app", help="Create a default app."),  # noqa: FBT001
+    web: bool = typer.Option(False, "--web", help="Create a website."),  # noqa: FBT001
+    api: bool = typer.Option(False, "--api", help="Create an API."),  # noqa: FBT001
+    worker: bool = typer.Option(False, "--worker", help="Create a worker."),  # noqa: FBT001
+    version: Annotated[  # noqa: ARG001
+        bool | None, typer.Option("--version", callback=version_callback, help="Show the version.")
+    ] = None,
 ):
     """
     Create a new Django project.
