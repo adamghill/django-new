c1d9b7956e58f46187e656f71f47b6936f8a51a7
Eric Matthes <ehmatthes@gmail.com>
2025-12-03 08:47:59 -0500

    Much cleaner structure for main summary function.

--- src/django_new/summary_utils.py
+++ src/django_new/summary_utils.py
@@ -15,35 +15,44 @@ def write_friendly_summary(project_name, folder_path):
     What's the rationale?
     Where can the user learn more?
     """
+    # Read base template string.
     path_summary_template = Path(__file__).parent / "friendly_summary.html"
     html_string = path_summary_template.read_text()
 
     # Build context dictionary.
     context = {
         "project_name": project_name,
-        "ts_created": datetime.now(timezone.utc).strftime("%m/%d/%Y %H:%M:%S")
+        "ts_created": datetime.now(timezone.utc).strftime("%m/%d/%Y %H:%M:%S"),
+        "tree_string": _get_tree_string(folder_path)
     }
-
-    # Generate project tree as a string that can be written to file.
-    tree = Tree(
-        f":open_file_folder: [link file://{folder_path}]{folder_path}",
-        guide_style="",
-    )
-    walk_directory(folder_path, tree)
-    buffer = StringIO()
-    file_console = Console(file=buffer, force_terminal=False, color_system=None)
-    file_console.print(tree)
-    context["tree_string"] = buffer.getvalue()
 
     # Make replacements.
     for key, value in context.items():
         html_string = html_string.replace(f"{{{{{key}}}}}", value)
 
-    # Write summary to file.
+    # Write customized summary to file.
     dest_path = folder_path / "friendly_summary.html"
     dest_path.write_text(html_string)
 
 
+# --- Helpers ---
+
+def _get_tree_string(folder_path):
+    """Get project directory structure as a string."""
+    tree = Tree(
+        f":open_file_folder: [link file://{folder_path}]{folder_path}",
+        guide_style="",
+    )
+    walk_directory(folder_path, tree)
+
+    buffer = StringIO()
+    file_console = Console(file=buffer, force_terminal=False, color_system=None)
+    file_console.print(tree)
+
+    return buffer.getvalue()
+
+
+# This is copied from cli.py to avoid circular import.
 def walk_directory(directory: Path, tree: Tree) -> None:
     """Recursively build a Tree with directory contents."""
 
